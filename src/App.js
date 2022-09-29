1e3c1f0888769ad07c8d282a3ad49a6fe9fc0e8a
MiguelLourenco <87149045+MigosLocus@users.noreply.github.com>
2022-09-29 23:30:10 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,6 @@ class App extends Component {
   state = {
     characters: [
       {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
         name: 'Dennis',
         job: 'Bartender',
       },
